99a6f2e54579da99b728b11ea5bb93b83df4b6ae
jsiewdass <jsiewdass@gmail.com>
2024-10-02 16:00:08 -0400

    se crea modulo de user

--- backend/app-core/src/app.module.ts
+++ backend/app-core/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -11,7 +12,8 @@ import { MongooseModule } from '@nestjs/mongoose';
       ttl: parseInt(process.env.RATE_LIMIT_TTL),
       limit: parseInt(process.env.RATE_LIMIT)
     }]),
-    MongooseModule.forRoot(process.env.DB_URI)
+    MongooseModule.forRoot(process.env.DB_URI),
+    UserModule
   ],
   providers: [AppService],
 })
---
+++ backend/app-core/src/user/schemas/user.schema.ts
@@ -0,0 +1,28 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Types } from "mongoose";
+
+export type UserDocument = User & Document;
+
+@Schema()
+export class User {
+    @Prop()
+    firstName: string;
+
+    @Prop()
+    lastName: string;
+
+    @Prop({
+        required: true,
+        unique: true
+    })
+    email: string;
+
+    @Prop({ required: true })
+    password: string;
+
+    // @Prop({type: [{ type: Types.ObjectId, ref: 'Wallet'}]})
+    // wallets: [Wallet];
+}
+
+
+export const UserSchema = SchemaFactory.createForClass(User);
